9f79416fc290fda87224432aba619aa926a13b07
tom <tom@vpn12-client-172-20-217-109.princeton.edu>
2023-01-22 14:16:43 -0500

    file tracking

---
+++ setup.py
@@ -0,0 +1,32 @@
+
+import setuptools
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='weathergen',
+    version='2.1.2',
+    description="Generates weather",
+    long_description=long_description,
+    author="Thomas Morris",
+    author_email='thomasmorris@princeton.edu',
+    url='https://github.com/thomaswmorris/weathergen',
+    python_requires='>=3.7',
+    packages=setuptools.find_packages(exclude=['docs', 'tests']),
+    include_package_data=True,
+    package_data={
+        'weathergen': [
+            # When adding files here, remember to update MANIFEST.in as well,
+            # or else they will not be included in the distribution on PyPI!
+            # 'path/to/data_file',
+        ]
+    },
+    install_requires=['numpy', 'scipy', 'pandas', 'datetime', 'h5py'],
+    license="BSD (3-clause)",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+)
