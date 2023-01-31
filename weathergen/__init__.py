4ee5e8a60de0bca6c9c57636d8c9a3277ecffe6d
tom <tom@10-4-22-168.user.vpn.bnl.local>
2023-01-31 18:31:22 -0500

    better quantiles

--- weathergen/__init__.py
+++ weathergen/__init__.py
@@ -23,7 +23,9 @@ def generate(**kwargs): return Weather(**kwargs)
 
 class Weather():
 
-    def __init__(self, site=None, time=None, verbose=False):
+    def __init__(self, site=None, time=None, verbose=False, mode='random'):
+
+        # mode is one of 'random', 'median'
 
         if site is None: site = 'princeton' # if no site is supplied, use 'princeton'
         if time is None: time = ttime.time() # if no time is supplied, use current time
@@ -31,33 +33,38 @@ class Weather():
         if not site in sites.index:
             raise ValueError(f"The site \'{site}\' is not supported. Available sites are:\n\n{site_string}")
     
+        if not mode in ['random', 'median']:
+            raise ValueError(f"mode must be one of ['random', 'median']")
+
         self.site, self.time, self.verbose = site, np.atleast_1d(time), verbose
         self.entry = sites.loc[self.site]
         self.lat, self.lon, self.alt = sites.loc[self.site, ['latitude', 'longitude', 'altitude']]
 
         self.quantiles = self.Quantiles(np.linspace(0,1,101))
-        self.generate()
+        self.generate(mode=mode)
 
         # define some stuff
         if 'S' in self.entry.type:
 
-            self.rain_precipitation_rate[self.rain_precipitation_rate < 1e-1] = 0 # we ignore precipitation less than 0.01 mm/hr
-            self.snow_precipitation_rate[self.snow_precipitation_rate < 1e-1] = 0
+            self.rain_precipitation_rate[self.rain_precipitation_rate < 1e-3] = 0 # we ignore precipitation less than 0.01 mm/hr
+            self.snow_precipitation_rate[self.snow_precipitation_rate < 1e-3] = 0
+            self.quantiles.rain_precipitation_rate[self.quantiles.rain_precipitation_rate < 1e-3] = 0
+            self.quantiles.snow_precipitation_rate[self.quantiles.snow_precipitation_rate < 1e-3] = 0
             
         if 'P' in self.entry.type:
 
             self.cloud_cover = np.minimum(1, np.maximum(0, self.cloud_cover))
 
             for k in ['water_vapor', 'ozone', 'ice_water', 'snow_water', 'rain_water', 'liquid_water']:
-                setattr(self, f'column_{k}', np.trapz(getattr(self, k), self.height, axis=0))
+                setattr(self, f'column_{k}', np.trapz(getattr(self, k), self.height, axis=0)[None])
 
-        self.wind_bearing              = np.degrees(np.arctan2(self.wind_east, self.wind_north) + np.pi)
-        self.wind_speed                = np.sqrt(np.square(self.wind_east) + np.square(self.wind_north))
+        self.wind_bearing      = np.degrees(np.arctan2(self.wind_east, self.wind_north) + np.pi)
+        self.wind_speed        = np.sqrt(np.square(self.wind_east) + np.square(self.wind_north))
 
-        self.relative_humidity         = np.minimum(100, np.maximum(1, utils.AH_to_RH(self.temperature, self.water_vapor)))
-        self.dew_point                 = utils.get_dew_point(self.temperature, self.relative_humidity)
+        self.relative_humidity = np.minimum(100, np.maximum(1, utils.AH_to_RH(self.temperature, self.water_vapor)))
+        self.dew_point         = utils.get_dew_point(self.temperature, self.relative_humidity)
         
-    def generate(self):
+    def generate(self, mode):
 
         filename = f'{base}/site_data/{self.site}.h5'
         self._gen_data = {}
@@ -71,8 +78,8 @@ class Weather():
         n_quantiles = len(self._gen_data['q'])
         n_features  = len(self._gen_data['eigenmodes'])
 
-        n_gen       = len(gen_time)
-        f_gen       = np.fft.fftfreq(n_gen, dt_gen)
+        n_gen = len(gen_time)
+        f_gen = np.fft.fftfreq(n_gen, dt_gen)
 
         self.year_day = list(map(utils.get_utc_year_day, gen_time))
         self.day_hour = list(map(utils.get_utc_day_hour, gen_time))
@@ -92,53 +99,48 @@ class Weather():
         self._gen_data['quantiles'][yd_edge_index][:,dh_edge_index], 
         method='linear')((self.year_day, self.day_hour)).reshape(n_features * n_gen, n_quantiles, order='F')
 
-        if self.verbose: print('Generating seeds . . .')
-        
-        # azdft is the spectrum of absolute values for the z-score discrete fourier transform
-        GOOD_F_BINS = ~np.isnan(self._gen_data['azdft_binned'])
-        AZDFT       = np.c_[[sp.interpolate.interp1d(self._gen_data['azdft_freq'][g], azdft[g], fill_value=0, bounds_error=False, kind='cubic')(f_gen) 
-                                                    for azdft, g in zip(self._gen_data['azdft_binned'], GOOD_F_BINS)]]
+        if mode == 'random':
+            if self.verbose: print('Generating seeds . . .')
+            GOOD_F_BINS = ~np.isnan(self._gen_data['azdft_binned'])
+            AZDFT       = np.c_[[sp.interpolate.interp1d(self._gen_data['azdft_freq'][g], azdft[g], fill_value=0, bounds_error=False, kind='cubic')(f_gen) 
+                                                        for azdft, g in zip(self._gen_data['azdft_binned'], GOOD_F_BINS)]]
 
-        GEN_DFT = 2 * AZDFT * np.sqrt(n_gen / dt_gen) * np.exp(1j*np.random.uniform(low=0,high=2*np.pi,size=AZDFT.shape))
-        #print(GEN_DFT.shape)
-        GEN_V   = np.real(np.fft.ifft(GEN_DFT, axis=1))
-        GD      = np.matmul(self._gen_data['eigenmodes'], GEN_V)
+            GEN_DFT = 2 * AZDFT * np.sqrt(n_gen / dt_gen) * np.exp(1j*np.random.uniform(low=0,high=2*np.pi,size=AZDFT.shape))
+            GEN_V   = np.real(np.fft.ifft(GEN_DFT, axis=1))
+            GD      = np.matmul(self._gen_data['eigenmodes'], GEN_V)
 
-        gu  = np.swapaxes(GD, 0, -1).reshape(n_features * n_gen, order='F')
+        else:
+            GD = np.zeros((self._gen_data['eigenmodes'].shape[0], n_gen))
+            
+        gu = np.swapaxes(GD, 0, -1).reshape(n_features * n_gen, order='F')
 
         bin_data = sp.interpolate.interp1d(self._gen_data['z'], np.arange(n_quantiles), 
                                         bounds_error=False, fill_value='extrapolate')(gu)
 
         bin_data = np.minimum(np.maximum(bin_data, 0), n_quantiles - 2)
 
-        DATA  = qu[np.arange(len(gu)), bin_data.astype(int)]# + dqu[np.arange(len(gu)), bin_whole] * bin_part).reshape(n_gen, n_features, order='F')
+        DATA  = qu[np.arange(len(gu)), bin_data.astype(int)]
         DATA += (bin_data % 1) * (qu[np.arange(len(qu)), bin_data.astype(int) + 1] - DATA)
         DATA  = np.swapaxes(DATA.reshape(n_gen, n_features, order='F'), 0, 1)
 
-        split_DATA = np.split(DATA, np.cumsum(self._gen_data['gen_widths'].astype(int))[:-1])
-        split_QUANTILES = np.split(np.swapaxes(qu.reshape(n_gen, n_features, n_quantiles, order='F'), 0, 1), np.cumsum(self._gen_data['gen_widths'].astype(int))[:-1])
+        QUANTILES = np.swapaxes(qu.reshape(n_gen, n_features, n_quantiles, order='F'), 0, 1)
 
-        #self.tog = gu.reshape(n_gen, n_features, order='F')
-        #self.toq = qu.reshape(n_gen, n_features, n_quantiles, order='F')
-        #print(self.DATA.shape, self.QUANTILES.shape)
-        for c, shaped, gd, qd in zip(self._gen_data['gen_labels'], 
-                                self._gen_data['gen_shaped'], 
-                                split_DATA,
-                                split_QUANTILES):
+        feature_splits = np.r_[0, np.cumsum(self._gen_data['gen_widths'].astype(int))]
+
+        for c, shaped, f0, f1 in zip(self._gen_data['gen_labels'], 
+                                     self._gen_data['gen_shaped'], 
+                                     feature_splits[:-1],
+                                     feature_splits[1:],
+                                     ):                                
         
-            _resampled = sp.interpolate.interp1d(gen_time, gd)(self.time)
-            _quantiles = sp.interpolate.interp1d(self._gen_data['q'], qd, axis=-1, kind='quadratic')(self.quantiles.q)
+            _resampled = sp.interpolate.interp1d(gen_time, DATA[f0:f1])(self.time)
+            _quantiles = sp.interpolate.interp1d(self._gen_data['q'], QUANTILES[f0:f1], axis=-1, fill_value='extrapolate', kind='linear')(self.quantiles.q)
 
             if not shaped: 
                 _resampled = _resampled.reshape(-1)
                 _quantiles = _quantiles.reshape(-1, self.quantiles.n_q)
-
-            #print(c, _resampled.shape, _quantiles.shape)
-
-            setattr(self, c, _resampled)
             setattr(self.quantiles, c, _quantiles.mean(axis=-2))
-
-            #print(c, _resampled.shape, getattr(self.quantiles, c).shape)
+            setattr(self, c, _resampled)
 
         if 'P' in self.entry.type: setattr(self, 'height', self._gen_data['height'])
         
