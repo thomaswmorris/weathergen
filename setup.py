e734110a75cb424219ce8242eda327f898dfa276
Thomas Morris <thomasmorris@princeton.edu>
2023-04-08 18:30:51 -0400

    actually read the requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-
+from os import path
 import setuptools
+
+here = path.abspath(path.dirname(__file__))
 
 with open('README.rst', 'r') as fh:
     long_description = fh.read()
+
+with open(path.join(here, "requirements.txt")) as requirements_file:
+    # Parse requirements.txt, ignoring any commented-out lines.
+    requirements = [line for line in requirements_file.read().splitlines() if not line.startswith("#")]
 
 setuptools.setup(
     name='weathergen',
@@ -12,13 +18,13 @@ setuptools.setup(
     author="Thomas Morris",
     author_email='thomasmorris@princeton.edu',
     url='https://github.com/thomaswmorris/weathergen',
-    python_requires='>=3.7',
+    python_requires='>=3.9.9',
     packages=setuptools.find_packages(exclude=['docs', 'tests']),
     include_package_data=True,
     package_data={
         'weathergen': []
     },
-    install_requires=['numpy', 'scipy', 'pandas', 'datetime', 'h5py'],
+    install_requires=requirements,
     license="BSD (3-clause)",
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
